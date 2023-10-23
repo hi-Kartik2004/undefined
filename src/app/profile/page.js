a54f53c7c085814cd73c5cc52f1e73a57c6bfd62
Kartikeya <kudlu2004@gmail.com>
2023-10-24 01:07:50 +0530

    Modifled profile page try:(deployment fix)

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,43 +1,54 @@
-"use client"
-import React from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from "next/navigation";
+"use client";
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { signOut } from 'next-auth/react';
 
-const page = () => {
+const Profile = () => {
+  const router = useRouter();
+  const { status, data: session } = useSession();
 
-    const router = useRouter();
-    const {status , data:session} = useSession();
-    if(status!='authenticated'){
-        router.push('/login')
+  useEffect(() => {
+    if (status === 'authenticated') {
+      // User is authenticated, you can access session data here
+      console.log(session?.user);
+    } else if (status === 'loading') {
+      // Session is still loading, you might want to show a loading spinner
+    } else {
+      // User is not authenticated, redirect to login page
+      router.push('/login');
     }
-    console.log(status)
+  }, [status, session, router]);
 
   return (
     <div>
-      hello! you are successfully authenticated
-    
-      <div className="center flex flex-col">
-        <div>
+      {status === 'authenticated' && (
+        <div className="center flex flex-col">
+          <div>
             User profile Image :
-           <Image src={session?.user?.image} width={200} height={180} />
-        </div>
+            <Image src={session?.user?.image} width={200} height={180} />
+          </div>
 
-        <div>
-            UserName : {session?.user?.name}
+          <div>
+            UserName: {session?.user?.name}
+          </div>
+          <div>
+            Email: {session?.user?.email}
+          </div>
         </div>
-        <div>
-           Email : {session?.user?.email}
-        </div>
-      </div>
+      )}
 
-    <button className="p-3 m-5 bg-white text-red-500 rounded-md hover:bg-red-300 hover:text-white"  onClick={()=>signOut()}>
-        Logout
-    </button>
-
+      {status === 'authenticated' && (
+        <button
+          className="p-3 m-5 bg-white text-red-500 rounded-md hover:bg-red-300 hover:text-white"
+          onClick={() => signOut()}
+        >
+          Logout
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Profile;
