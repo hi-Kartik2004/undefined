31fcc5abcfde0f69fe5abb31b9aa70f59e2a8cca
Mohammed Numan Raza <89739573+Numan71417@users.noreply.github.com>
2023-10-24 09:45:51 +0530

    Update page3.js

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { useSession } from 'next-auth/react'
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import Image from 'next/image';
 import { signOut } from 'next-auth/react';
 
