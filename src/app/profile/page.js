b7f70c9a20cf2dd16ab3400ae2792f31b3666852
Kartikeya <kudlu2004@gmail.com>
2023-10-26 09:57:20 +0530

    test4

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,43 +1,28 @@
-"use client"
-import React from 'react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from "next/navigation";
-import Image from 'next/image';
-import { signOut } from 'next-auth/react';
+import React, { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
-const page = () => {
-
+const Page = () => {
     const router = useRouter();
-    const {status , data:session} = useSession();
-    if(status!='authenticated'){
-        router.push('/login')
+    const { status, data: session } = useSession();
+
+    useEffect(() => {
+        if (status !== 'authenticated') {
+            router.push('/login');
+        }
+    }, [status, router]);
+
+    if (status !== 'authenticated') {
+        // You might want to return a loading spinner or something while the session is being checked.
+        return null;
     }
-    console.log(status)
 
-  return (
-    <div>
-      hello! you are successfully authenticated
-    
-      {/* <div className="center flex flex-col">
+    return (
         <div>
-            User profile Image :
-           <Image src={session?.user?.image} width={200} height={180} />
+            hello! you are successfully authenticated
+            {/* Your content here */}
         </div>
+    );
+};
 
-        <div>
-            UserName : {session?.user?.name}
-        </div>
-        <div>
-           Email : {session?.user?.email}
-        </div>
-      </div>
-
-    <button className="p-3 m-5 bg-white text-red-500 rounded-md hover:bg-red-300 hover:text-white"  onClick={()=>signOut()}>
-        Logout
-    </button> */}
-
-    </div>
-  )
-}
-
-export default page
+export default Page;
