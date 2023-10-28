c65d4efc43ebae05f352510e4d16382680dad5b8
Kartikeya <kudlu2004@gmail.com>
2023-10-28 13:32:19 +0530

    Testing Google auth

--- src/app/components/GoogleAuth.jsx
+++ src/app/components/GoogleAuth.jsx
@@ -3,26 +3,34 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import session from "express-session";
 
 const GoogleAuth = () => {
   const router = useRouter();
+
   const handleGoogleLogin = async () => {
-    const result = await signIn("google", {
-      redirect: false,
-      callbackUrl: "/",
-    });
+    try {
+      const result = await signIn("google", {
+        redirect: false,
+        callbackUrl: "/creator/1",
+      });
 
-    // Check if the login was successful and user details are available
-    if (result?.error) {
-      // Handle login error if needed
-      console.error("Google login failed:", result.error);
-    } else if (result?.user) {
-      const username = result.user.email.split("@")[0];
-      const userWithUsername = { ...result.user, username };
-      sessionStorage.setItem("user", JSON.stringify(userWithUsername));
+      // Check if the login was successful and user details are available
+      if (result?.error) {
+        // Handle login error if needed
+        console.error("Google login failed:", result.error);
+      } else if (result?.user) {
+        const username = result.user.email.split("@")[0];
+        const userWithUsername = { ...result.user, username };
 
-      router.push(`/creator/${result.user.email.split("@")[0]}`);
+        // Update sessionStorage with user and token details
+        sessionStorage.setItem("user", JSON.stringify(userWithUsername));
+        sessionStorage.setItem("token", result.user.accessToken);
+
+        // Redirect to the user's profile page after successful login
+        router.push(`/creator/${username}`);
+      }
+    } catch (error) {
+      console.error("Google login error:", error);
     }
   };
 
